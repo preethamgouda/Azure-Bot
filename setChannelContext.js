451b0359718c4c8c6e9bb274c25fb723560480dd
preethamgowda-dev <72795407+preethamgowda-dev@users.noreply.github.com>
2022-01-15 20:49:55 +0530

    Delete setChannelContext.js

--- setChannelContext.js
+++
@@ -1,8 +0,0 @@
-const parseSlackMessage = message => message.replace(process.env.SLACK_HANDLE, '');
-
-module.exports.botbuilder = (session, next) => {
-  if (session.message.address.channelId === 'slack') {
-    session.message.text = parseSlackMessage(session.message.text);
-  }
-  next();
-};
